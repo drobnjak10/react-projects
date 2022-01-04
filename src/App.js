cb910067e262efd45e583ac6e033a7b9e924788f
drobnjakdavid <drobnjakdavid@gmail.com>
2022-01-04 09:20:00 +0100

    =Final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import './App.css';
+import List from './List';
+import data from './data.js'
 
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        React
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{ people.length } birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear all</button>
+      </section>
+    </main>
   );
 }
 
