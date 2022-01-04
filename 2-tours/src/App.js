8389ba048ab786ea0f35c3595e21aba890e924a0
drobnjakdavid <drobnjakdavid@gmail.com>
2022-01-04 12:49:09 +0100

    =Finish project

--- 2-tours/src/App.js
+++ 2-tours/src/App.js
@@ -1,12 +1,51 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Loading from './Loading';
+import Tours from './Tours';
+
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  const removeTour = (id) => {
+     const newTours = tours.filter(tour => tour.id !== id);
+     setTours(newTours);
+  }
+
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(`https://api.allorigins.win/raw?url=${url}`);
+      const tours = await response.json();
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+
+
+  useEffect(() => {
+    fetchTours();
+  }, [])
+
+
+  if (loading) {
+    return <main>
+      <Loading />
+    </main>
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        React App
-      </header>
-    </div>
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
   );
 }
 
